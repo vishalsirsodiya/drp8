201949cf20dcee6de997b12d80734b8e4859ae57
vishalsirsodiya <vishal.sirsodiya23@gmail.com>
2018-06-07 11:12:59 +0530

    Drupal code commit fisrt time

---
+++ sites/drp8.dd/files/php/twig/5b178dfb23bd2_menu--account.html.twig_lPhyLlenAq_Ta2UW8jeUEU4Cy/O0XtTgqE_1Cuz_5KIKskSy5kN6GCSvx3S3Gdhl8Z6b0.php
@@ -0,0 +1,81 @@
+<?php
+
+/* themes/contrib/bootstrap/templates/menu/menu--account.html.twig */
+class __TwigTemplate_5bc3e199f519c7ded148272665cecb57032c1eed8a232def9df53b54d4251d45 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 18
+        $this->parent = $this->loadTemplate("menu.html.twig", "themes/contrib/bootstrap/templates/menu/menu--account.html.twig", 18);
+        $this->blocks = array(
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "menu.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("set" => 20);
+        $filters = array("clean_class" => 22);
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array('set'),
+                array('clean_class'),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 20
+        $context["classes"] = array(0 => "menu", 1 => ("menu--" . \Drupal\Component\Utility\Html::getClass(        // line 22
+($context["menu_name"] ?? null))), 2 => "nav", 3 => "navbar-nav", 4 => "navbar-right");
+        // line 18
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/contrib/bootstrap/templates/menu/menu--account.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  51 => 18,  49 => 22,  48 => 20,  11 => 18,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "themes/contrib/bootstrap/templates/menu/menu--account.html.twig", "D:\\DevDesktop\\drp8\\themes\\contrib\\bootstrap\\templates\\menu\\menu--account.html.twig");
+    }
+}
---
+++ sites/drp8.dd/files/php/twig/5b178dfb23bd2_page-title.html.twig_ig2GHIGXsZgkaVBx_stR3E-fk/eZHMnn6o-VC09dSUbNC_zI2A7QV_LajQSk9GAvbKJsE.php
@@ -0,0 +1,81 @@
+<?php
+
+/* themes/contrib/bootstrap/templates/system/page-title.html.twig */
+class __TwigTemplate_85896b7136320eb2ca2d942a36c20a1da66409a3527857f5ff69026c0b151763 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("if" => 19);
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array('if'),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 19
+        if (($context["title"] ?? null)) {
+            // line 20
+            echo "  <h1";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["title_attributes"] ?? null), "addClass", array(0 => "page-header"), "method"), "html", null, true));
+            echo ">";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["title"] ?? null), "html", null, true));
+            echo "</h1>
+";
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/contrib/bootstrap/templates/system/page-title.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  45 => 20,  43 => 19,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "themes/contrib/bootstrap/templates/system/page-title.html.twig", "D:\\DevDesktop\\drp8\\themes\\contrib\\bootstrap\\templates\\system\\page-title.html.twig");
+    }
+}
